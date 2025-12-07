64d511a9e6586d9da36873793656f04d45a7a294
Andrew M <mnandrei90@gmail.com>
2025-12-07 18:46:49 +0200

    Implemented parsing

---
+++ src/parse.c
@@ -0,0 +1,95 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#include "common.h"
+#include "parse.h"
+
+int create_db_header(int fd, struct dbheader_t **headerOut) {
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL) {
+        printf("Failed to allocate memory for file header.\n");
+        return STATUS_ERROR;
+    }
+
+    header->version = 0x1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *headerOut = header;
+
+    return STATUS_SUCCESS;
+}
+
+int validate_db_header(int fd, struct dbheader_t **headerOut) {
+    if (fd < 0) {
+        printf("Invalid file descriptor\n");
+        return STATUS_ERROR;
+    }
+
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL) {
+        printf("Failed to allocate memory for file header.\n");
+        return STATUS_ERROR;
+    }
+
+    if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t)) {
+        perror("read");
+        free(header);
+
+        return STATUS_ERROR;
+    }
+
+    header->version = ntohs(header->version);
+    header->count = ntohs(header->count);
+    header->magic = ntohl(header->magic);
+    header->filesize = ntohl(header->filesize);
+
+    if (header->version != 1) {
+        printf("Improper header version\n");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    if (header->magic != HEADER_MAGIC) {
+        printf("Improper header magic\n");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    struct stat dbstat = {0};
+    fstat(fd, &dbstat);
+    if (header->filesize != dbstat.st_size) {
+        printf("Corrupted database file\n");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    *headerOut = header;
+
+    return STATUS_SUCCESS;
+}
+
+int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut){}
+
+void output_file(int fd, struct dbheader_t * header, struct employee_t **employeesOut) {
+    if (fd < 0) {
+        printf("Invalid file descriptor\n");
+        return;
+    }
+
+    header->magic = htonl(header->magic);
+    header->filesize = htonl(header->filesize);
+    header->count = htons(header->count);
+    header->version = htons(header->version);
+
+    lseek(fd, SEEK_SET, 0);
+
+    write(fd, header, sizeof(struct dbheader_t));
+}
